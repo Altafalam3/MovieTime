2a94f9bd7d8ad8cf26686eb386c80bf5735b4777
Altafalam3 <altaf.alam0032@gmail.com>
2024-04-15 20:49:29 +0530

    offline ho jaa

--- client/src/redux/userApi.js
+++ client/src/redux/userApi.js
@@ -89,10 +89,10 @@ export const userApi = createApi({
     }),
   }),
   // Add the cache headers
-  refetchOnMountOrArgChange: false,
-  refetchOnReconnect: false,
-  refetchOnFocus: false,
-  keepUnusedDataFor: 60 * 60 * 1000, // Keep data in the cache for 1 hour
+  // refetchOnMountOrArgChange: false,
+  // refetchOnReconnect: false,
+  // refetchOnFocus: false,
+  // keepUnusedDataFor: 60 * 60 * 1000, // Keep data in the cache for 1 hour
 
 });
 
